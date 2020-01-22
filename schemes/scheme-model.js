9f5c6e7a669a463195ab58c4b02e0342de54d232
jordan <jordan@jordans-MacBook-Air.local>
2020-01-22 20:54:44 +0700

    db config helpers and queries

---
+++ schemes/scheme-model.js
@@ -0,0 +1,45 @@
+const db = require("../data/db-config");
+
+module.exports = {
+  find,
+  findById,
+  findSteps,
+  add,
+  update,
+  remove
+};
+
+function find() {
+  return db("schemes").select();
+}
+
+function findById(id) {
+  return db("schemes")
+    .where({ id })
+    .first();
+}
+
+function findSteps(id) {
+  return db("steps")
+    .where({ id })
+    .first();
+}
+
+async function add(schemeData) {
+  const [id] = await db("schemes").insert(schemeData);
+  return db("schemes")
+    .where({ id })
+    .first();
+}
+
+function update(changes, id) {
+  return db("schemes")
+    .where({ id })
+    .update(changes);
+}
+
+function remove(id) {
+  return db("schemes")
+    .where({ id })
+    .del();
+}
